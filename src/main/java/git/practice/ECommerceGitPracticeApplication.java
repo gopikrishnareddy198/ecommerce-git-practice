6124da850a3d74939d74aaba45fa9beccc9b5e57
Gopikrishna Reddy <gopikrishnareddy198@gmail.com>
2022-04-15 15:13:02 -0500

    Initial commit

---
+++ src/main/java/git/practice/ECommerceGitPracticeApplication.java
@@ -0,0 +1,13 @@
+package git.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceGitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceGitPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/practice/ECommerceGitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package git.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceGitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
