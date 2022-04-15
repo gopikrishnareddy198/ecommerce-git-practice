16d79925d76f3c2d6ce417784837003ea10a05c9
Gopikrishna Reddy <gopikrishnareddy198@gmail.com>
2022-04-15 15:49:05 -0500

    Created Service lahyer

---
+++ src/main/java/git/practice/users/service/UserService.java
@@ -0,0 +1,12 @@
+package git.practice.users.service;
+
+import git.practice.users.model.User;
+
+public class UserService {
+
+
+    public void validatedetailsAndCreateUser(User user) {
+
+
+    }
+}
